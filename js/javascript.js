18642b5d1290ac34ebe6c47a582ac32172b511c9
soobinn <76994237+soobinn@users.noreply.github.com>
2023-07-25 17:28:04 +0900

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,145 +1,5 @@
 
 window.onload = function () {
-    // 여기에다가 script 코드 작성
-    // var count = 0;
-
-    // var next = document.querySelector('.next');
-    // var before = document.querySelector('.before');
-    // var img = document.querySelector('.slide_img');
-    // var list = document.querySelector('.slide_list');
-
-    // next.addEventListener('click', function () {
-
-    //     if (count == 0) {
-
-    //         count += 1;
-    //         list.style.marginLeft = `-${count}00%`;
-    //         list.style.transition = '0.5s';
-    //         // console.log(count)
-    //     } else if (count == 1) {
-
-    //         count += 1;
-    //         list.style.marginLeft = `-${count}00%`;
-    //         // console.log(count)
-
-    //     } else if (count == 2) {
-    //         count = 0;
-    //         list.style.marginLeft = `-${count}00%`;
-    //         // console.log(count)
-
-    //     }
-
-    // });
-    // before.addEventListener('click', function () {
-
-    //     if (count == 2) {
-
-    //         count -= 1;
-    //         list.style.marginLeft = `-${count}00%`;
-    //         // console.log(count)
-    //     } else if (count == 1) {
-
-    //         count -= 1;
-    //         list.style.marginLeft = `-${count}00%`;
-    //         // console.log(count)
-
-    //     } else if (count == 0) {
-    //         count = 2;
-    //         list.style.marginLeft = `-${count}00%`;
-    //         // console.log(count);
-
-    //     }
-
-    // });
-
-    // //  자동슬라이드 fadein으로 만들고싶음
-    // setInterval(function () {
-
-
-    //     if (count == 0) {
-
-    //         count += 1;
-    //         list.style.marginLeft = `-${count}00%`;
-    //         list.style.transition = "0.8s";
-    //         // console.log(count)
-    //     } else if (count == 1) {
-
-    //         count += 1;
-    //         list.style.marginLeft = `-${count}00%`;
-    //         // console.log(count)
-
-    //     } else if (count == 2) {
-    //         count = 0;
-    //         list.style.marginLeft = `-${count}00%`;
-    //         // console.log(count);
-
-    //     }
-    // }, 5000)
-
-
-    // class CardFlipOnScroll {
-    //     constructor(wrapper, sticky) {
-    //         this.wrapper = wrapper;
-    //         this.sticky = sticky;
-    //         this.cards = sticky.querySelectorAll('.card video');
-    //         this.length = this.cards.length;
-
-    //         this.start = 0
-    //         this.end = 0
-    //         this.step = 0
-    //     }
-
-    //     init() {
-    //         this.start = this.wrapper.offsetTop
-    //         this.end = this.wrapper.offsetTop + this.wrapper.offsetHeight - innerHeight
-    //         this.step = (this.end - this.start) / (this.length *1.3)
-    //     }
-
-    //     animate() {
-    //         this.cards.forEach((card, i) => {
-    //             const s = this.start + this.step * i
-    //             const e = s + this.step * (this.length/10)
-
-    //             if (scrollY <= s) {
-    //                 card.style.marginTop = `
-
-
-    //                 ${280 + -(scrollY - (e - this.step)) / this.step * 180}px
-    //               `
-
-
-    //             } else if (scrollY > s && scrollY <= e - this.step) {
-    //                 card.style.marginTop = `
-
-
-    //                 ${280 + -(scrollY - (e - this.step)) / this.step * 300}px
-    //               `
-
-    //             } else if (scrollY > e - this.step && scrollY <= e) {
-
-    //                 card.style.marginTop = `
-
-
-    //                 ${300 + -(scrollY - (e - this.step)) / this.step * 300}px
-    //           `
-
-
-    //             } else if (scrollY > e) {
-    //                 card.style.marginTop = `0px`
-    //             }
-    //         })
-    //     }
-    // }
-
-    // const mainContent1 = document.querySelector('.main-content-1')
-    // const sticky = document.querySelector('.sticky')
-    // const cardFlipOnScroll = new CardFlipOnScroll(mainContent1, sticky)
-    // cardFlipOnScroll.init()
-
-    // window.addEventListener('scroll', () => {
-    //     cardFlipOnScroll.animate()
-    // })
-
 
     // 메뉴바 스크롤시
     let menu = document.querySelector('header');
@@ -147,25 +7,25 @@ window.onload = function () {
     let menu3 = document.querySelector('.bg');
     window.addEventListener("scroll", (event) => {
         let scrollY = this.scrollY;
-      
 
-        if(scrollY >= 100){
-           menu.classList.add("down");
-           menu2.classList.add("down2");
-           menu3.classList.add("down3");
 
-        }else{
+        if (scrollY >= 100) {
+            menu.classList.add("down");
+            menu2.classList.add("down2");
+            menu3.classList.add("down3");
+
+        } else {
 
             menu.classList.remove("down");
             menu2.classList.remove("down2");
             menu3.classList.remove("down3");
         }
-       
+
     });
 
 
     // 로그인 회원가입
-    document.querySelector('.up').addEventListener('click',function(){
+    document.querySelector('.up').addEventListener('click', function () {
 
 
         document.querySelector('.login').classList.toggle('opacity');
@@ -174,7 +34,7 @@ window.onload = function () {
 
     });
 
-    
+
     var slides = document.querySelector('.main_slide');
     var slide = document.querySelectorAll('.main_slide_list');
     var crrentIdx = 0;
@@ -265,7 +125,7 @@ window.onload = function () {
         } else if (toggle == false) {
             clearInterval(mainSlideInterval)
             toggle = !toggle;
-           document.querySelector('.toggle_img').src = "imgs/pause.png";
+            document.querySelector('.toggle_img').src = "imgs/pause.png";
         }
 
     });
--- js/jquery.js
+++ js/jquery.js
@@ -1,25 +1,25 @@
 jQuery(document).ready(function () {
-    
-    
+
+
     $('.content4_box_slide a:gt(0)').hide();
-    setInterval(function(){
-        
+    setInterval(function () {
+
         $('.content4_box_slide a:first-child').fadeOut(500)
-        .next('a')
-        .fadeIn(500)
-        .end()
-        .appendTo('.content4_box_slide')
-        
-    },2500);
-    
-    $('.main_menu_list').mouseover(function(){
-    
-    
-    
+            .next('a')
+            .fadeIn(500)
+            .end()
+            .appendTo('.content4_box_slide')
+
+    }, 2500);
+
+    $('.main_menu_list').mouseover(function () {
+
+
+
         $('.sub_menu').stop().slideDown(400);
         $('.bg').stop().slideDown(500);
-    }).mouseout(function(){
-    
+    }).mouseout(function () {
+
         $('.sub_menu').stop().slideUp(400);
         $('.bg').stop().slideUp(500);
     });
